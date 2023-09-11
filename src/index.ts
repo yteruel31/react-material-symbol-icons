cd6af507ecc613f4be8cd552e499456f69c5a770
Yoann TERUEL <yoann.teruel@gmail.com>
2023-09-11 22:22:50 +0200

    chore: export correctly IconKey type from the global export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * from './components/MaterialSymbol';
 
-export { IconKey } from './components/Icons/generated/generated-icon-keys';
+export type { IconKey } from './components/Icons/generated/generated-icon-keys';
